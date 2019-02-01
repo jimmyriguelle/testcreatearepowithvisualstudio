3d819f0d68c2b52c1451b06a6d51231263483851
Jimmy Riguelle <jimmy.riguelle@sogeti.com>
2019-02-01 20:13:23 +0100

    1. create the repo 2. create a new solution inside the repo 3. choose change to commiy

---
+++ UnitTestProjecttest/UnitTestProjecttest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProjecttest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProjecttest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("22a309f0-3e26-4fa9-b116-d4afe5c22267")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
